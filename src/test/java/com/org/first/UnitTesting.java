fab70f3e2ff5fbf47c422c3dfca06cdf3bb47f9f
anilyadav <anil01.yadav@gmail.com>
2017-06-28 17:37:13 +0530

    Added all the required files

---
+++ src/test/java/com/org/first/UnitTesting.java
@@ -0,0 +1,36 @@
+package com.org.first;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+/**
+ * Created by E003690 on 6/28/2017.
+ */
+public class UnitTesting {
+
+    public WebDriver driver;
+
+    @BeforeTest
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\E003690\\Downloads\\chromedriver.exe");
+        driver=new ChromeDriver();
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void verifyLoginPage(){
+       driver.get("https://accounts.google.com/");
+    }
+
+    @AfterTest
+    public void tearDown(){
+        driver.quit();
+    }
+
+}
